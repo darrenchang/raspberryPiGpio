75f614c1c542529e2a2b17923c98e736ca63fbc0
Darren Chang <darrenchang951@gmail.com>
2017-08-27 02:57:30 +1000

    added a function to alternate the lights slowly

--- alternatingLED/alternatingLED.py
+++ alternatingLED/alternatingLED.py
@@ -1,8 +1,8 @@
-import RPi.GPIO as GPIO
 import time
 import thread
 import click
 
+# Setup GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)
 
@@ -10,6 +10,10 @@ key = ''
 
 
 def wait_for_key_press():
+    """
+    listen for keypress and assign the pressed key char to a variable
+    :return:
+    """
     try:
         while True:
             global key
@@ -39,6 +43,18 @@ def both_light():
         GPIO.output(8, 1)
         time.sleep(0.0001)
 
+
+def alternate_slowly():
+    """
+    alternate the lights slowly
+    :return:
+    """
+    while key == 's':
+        GPIO.output(8, 0)
+        time.sleep(0.5)
+        GPIO.output(8, 1)
+        time.sleep(0.5)
+
 try:
     # start a new thread to listen for keypress
     thread.start_new_thread(wait_for_key_press, ())
@@ -46,6 +62,7 @@ try:
     while True:
         one_light()
         both_light()
+        alternate_slowly()
         # add a small delay in the main loop so the other threads can run
         # [there should be a better way to solve this problem]
         time.sleep(0.001)
