1cae829d136d85f2fe904c37a09e1b06fe2b170e
Darren Chang <darrenchang951@gmail.com>
2017-08-27 23:06:28 +1000

    Fixed issue where you need to quit key listener and the main program to exit.

--- alternatingLED/alternatingLED.py
+++ alternatingLED/alternatingLED.py
@@ -8,6 +8,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)
 
 key = ''
+stop = False
 
 
 def wait_for_key_press():
@@ -15,16 +16,17 @@ def wait_for_key_press():
     listen for keypress and assign the pressed key char to a variable
     :return:
     """
+
     try:
         while True:
             global key
+            global stop
             key = click.getchar()
-            print key
-    except KeyboardInterrupt:
-        pass
-    finally:
-        pass
+            print key + " pressed"
 
+    except KeyboardInterrupt:
+        stop = True
+        print "A keyboard interrupt has been noticed"
 
 def one_light():
     if key in ['r', 'R']:
@@ -38,11 +40,11 @@ def one_light():
 
 
 def both_light():
-    while key in ['b', 'B']:
+    while key in ['b', 'B'] and not stop:
         GPIO.output(8, 0)
-        time.sleep(0.0001)
+        time.sleep(0.001)
         GPIO.output(8, 1)
-        time.sleep(0.0001)
+        time.sleep(0.001)
 
 
 def alternate_slowly():
@@ -50,29 +52,29 @@ def alternate_slowly():
     alternate the lights slowly
     :return:
     """
-    while key in ['s', 'S']:
-        GPIO.output(8, 0)
-        time.sleep(0.5)
-        GPIO.output(8, 1)
-        time.sleep(0.5)
+
+    switch_speed = 10
+    while key in ['s', 'S'] and not stop:
+        if int(round(time.time())) % (switch_speed * 2) < switch_speed:
+            GPIO.output(8, 0)
+        else:
+            GPIO.output(8, 1)
+        time.sleep(0.1)
 
 try:
     # start a new thread to listen for keypress
     thread.start_new_thread(wait_for_key_press, ())
 
-    while True:
+    while not stop:
         one_light()
         both_light()
         alternate_slowly()
         # add a small delay in the main loop so the other threads can run
         # [there should be a better way to solve this problem]
-        time.sleep(0.001)
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
-    print "A keyboard interrupt has been notitced"
-
-except:
-    print "An error or exception has occured!"
+    print "A keyboard interrupt has been noticed"
 
 finally:
     GPIO.cleanup()
